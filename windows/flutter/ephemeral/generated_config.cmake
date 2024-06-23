bb622c51cb602dd3de0cd0ae8e5bd6cbeee900e8
darshanx234 <dvchauhan5566@gmail.com>
2024-06-23 10:59:55 +0000

    commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter pro\\fireconnct" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "PROJECT_DIR=D:\\flutter pro\\fireconnct"
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter pro\\fireconnct\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter pro\\fireconnct"
+  "FLUTTER_TARGET=D:\\flutter pro\\fireconnct\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iZDk4NmM1ZWQyMGE2MmRjMzRiNzcxOGM1MGFiYzc4MmJlYWU0YzMzLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter pro\\fireconnct\\.dart_tool\\package_config.json"
+)
